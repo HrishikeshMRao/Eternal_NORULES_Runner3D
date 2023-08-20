8a2a84d6d35207f4f6ddbc74170c706098cad72b
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2023-08-20 13:03:04 +0530

    added jumping and sliding features

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,32 +5,55 @@ using System.Collections.Generic;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float slideTime = 0.75f;
     public float horAcceleration = 10f;
     public float forwardAcceleration = 10f;
-    public float gravity = 9.8f;
+    public float gravity = 20f;
     public float deceleration = 20f;
-    public float forwardSpeed = 0f, horSpeed = 0f;
+    public float forwardSpeed = 0f, horSpeed = 0f, verticleSpeed = 0f;
     public float maxForwardSpeed = 30f, maxHorSpeed = 10f;
     public float difficultyBar = 300f;
     public bool isControlEnable = true;
     public GameManager gameManager;
     public TextMeshProUGUI scoreText;
     public Vector3 velocity;
+    public Animator animator;
     public CharacterController controller;
     [SerializeField] private float Rotation_speed;
+    private Vector3 controllerCenter;
     private Vector3 playerVelocity;
     private float inputMovement = 0;
+    private float inputVerticle = 0;
+    private float effectiveGravity; // Same as gravity, but will become dowble if while jumping, player slides
     private bool isGameOver = false;
     private Quaternion initialRotation;
+
     
     public void OnMovement(InputAction.CallbackContext value) 
     {
         inputMovement = value.ReadValue<float>();
     }
 
+    public void OnJump(InputAction.CallbackContext value)
+    {
+        inputVerticle = value.ReadValue<float>();
+
+        if(inputVerticle > 0 && controller.isGrounded && !animator.GetBool("isSliding")) {
+            verticleSpeed = 7f; // Took arbitary tested value for now
+            animator.SetBool("isJumping", true);
+            Debug.Log("jumping");
+        } else if(inputVerticle < 0 && controller.isGrounded) {
+            StartSlide(false);
+        } else if(inputVerticle < 0) {
+            StartSlide(true);
+        }
+    }
+
     void Start() 
     {
         controller = GetComponent<CharacterController>();
+
+        effectiveGravity = gravity;
     }
 
     void Update()
@@ -39,6 +62,7 @@ public class PlayerControl : MonoBehaviour
             gameManager.GameOver("isFalling");
             isGameOver = true;
         }
+
         if(isControlEnable)
             Move();
         else
@@ -75,11 +99,18 @@ public class PlayerControl : MonoBehaviour
         forwardSpeed += forwardAcceleration * Time.deltaTime;
         forwardSpeed = Mathf.Clamp(forwardSpeed, 0, maxForwardSpeed);
 
-        playerVelocity = Vector3.forward * forwardSpeed + Vector3.right * horSpeed + Vector3.down * gravity;
+        if(!controller.isGrounded)
+            verticleSpeed -= effectiveGravity * Time.deltaTime;
+
+        playerVelocity = Vector3.forward * forwardSpeed + Vector3.right * horSpeed + Vector3.up * verticleSpeed;
 
         velocity = controller.velocity;
         controller.Move(playerVelocity * Time.deltaTime);
-    }
+
+        if(controller.isGrounded)
+            animator.SetBool("isJumping", false);
+
+    }    
 
     private void rotation()
     {
@@ -93,5 +124,36 @@ public class PlayerControl : MonoBehaviour
         playerVelocity = Vector3.forward * forwardSpeed;
 
         controller.Move(playerVelocity * Time.deltaTime); 
+    }
+
+    void StartSlide(bool wasJumping)
+    {
+        controllerCenter = controller.center;
+        controllerCenter.y = -0.5f;
+
+        controller.center = controllerCenter;
+        controller.height = 1;
+
+        animator.SetBool("isJumping", false);
+        animator.SetBool("isSliding", true);
+
+        Invoke("StopSliding", slideTime);
+
+        if(wasJumping) {
+            effectiveGravity = 2 * gravity;
+        }
+    }
+
+    void StopSliding()
+    {
+        controllerCenter = controller.center;
+        controllerCenter.y = -0.05f;
+        
+        controller.center = controllerCenter;
+        controller.height = 1.9f;
+
+        animator.SetBool("isSliding", false);
+
+        effectiveGravity = gravity; 
     }
 }
